fda96c91df8db209ee263d91aafff3bbdb444d1e
Init project <congpgph26764@fpt.edu.vn>
2022-11-18 04:12:35 +0700

    Update quan ly loai san pham

--- app/src/main/java/com/example/duan_oder_doan/model/TheLoai.java
+++ app/src/main/java/com/example/duan_oder_doan/model/TheLoai.java
@@ -1,15 +1,25 @@
 package com.example.duan_oder_doan.model;
 
 public class TheLoai {
+    private String id;
     private int img_category;
     private String name_category;
 
     public TheLoai() {
     }
 
-    public TheLoai(int img_category, String name_category) {
+    public TheLoai(String id, int img_category, String name_category) {
+        this.id = id;
         this.img_category = img_category;
         this.name_category = name_category;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public int getImg_category() {
