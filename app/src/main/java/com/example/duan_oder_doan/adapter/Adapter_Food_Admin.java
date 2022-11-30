f9f363e466af14f1be4631ab199020e5f622b961
Init project <congpgph26764@fpt.edu.vn>
2022-11-30 20:36:14 +0700

    them popular

--- app/src/main/java/com/example/duan_oder_doan/adapter/Adapter_Food_Admin.java
+++ app/src/main/java/com/example/duan_oder_doan/adapter/Adapter_Food_Admin.java
@@ -1,17 +1,27 @@
 package com.example.duan_oder_doan.adapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.duan_oder_doan.HoaDonUser;
 import com.example.duan_oder_doan.R;
+import com.example.duan_oder_doan.TrangChuUser;
 import com.example.duan_oder_doan.model.SanPham;
+import com.example.duan_oder_doan.model.SanPham_Favorite;
+import com.example.duan_oder_doan.model.SanPham_Popular;
 import com.example.duan_oder_doan.model.TheLoai;
 import com.example.duan_oder_doan.view_holder.View_Holder_Category_Admin;
 import com.example.duan_oder_doan.view_holder.View_Holder_Food_Admin;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -47,6 +57,19 @@ public class Adapter_Food_Admin extends RecyclerView.Adapter<View_Holder_Food_Ad
         holder.tvCategoryFood.setText(sanPham.getCategory());
         holder.tvNoteFood.setText(sanPham.getNote_product());
         Picasso.get().load(sanPham.getImg_product()).into(holder.imgItemFood);
+        holder.imgItemFood.setOnClickListener(v ->{
+            SanPham_Popular sanPham_popular = new SanPham_Popular(sanPham.getImg_product(),sanPham.getName_product(), sanPham.getPrice_product(), sanPham.getNote_product());
+            FirebaseDatabase.getInstance().getReference("FoodPopulars")
+                    .child(sanPham.getName_product())
+                    .setValue(sanPham_popular).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(v.getContext(), "Add food popular successfully!", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        });
         holder.lineItemFood.setOnLongClickListener(v ->{
             callback.update(sanPham);
             return false;
